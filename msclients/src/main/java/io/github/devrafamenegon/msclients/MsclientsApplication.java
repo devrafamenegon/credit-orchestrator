5a2c75b087ba597830037102fcef7feb043a9c62
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2025-02-05 16:50:57 -0300

    feat: registering msclients into eureka server with random ports

--- msclients/src/main/java/io/github/devrafamenegon/msclients/MsclientsApplication.java
+++ msclients/src/main/java/io/github/devrafamenegon/msclients/MsclientsApplication.java
@@ -2,6 +2,7 @@ package io.github.devrafamenegon.msclients;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.CloudEurekaClient;
 
 @SpringBootApplication
 public class MsclientsApplication {
