cc96baf3197faab597965a021a38563408392722
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2025-02-05 01:23:09 -0300

    build: config discovery server

--- discoveryserver/src/main/java/io/github/devrafamenegon/discoveryserver/DiscoveryserverApplication.java
+++ discoveryserver/src/main/java/io/github/devrafamenegon/discoveryserver/DiscoveryserverApplication.java
@@ -2,8 +2,10 @@ package io.github.devrafamenegon.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoveryserverApplication {
 
 	public static void main(String[] args) {
